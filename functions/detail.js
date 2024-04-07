c10d02f4d74a95b8573c9a23b817bf0d590bcc92
Şevval <sevval@Sevval-MacBook-Pro.local>
2024-04-07 17:37:14 +0300

    details page

---
+++ functions/detail.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const restaurantId = queryParams.get("id");
+    if (restaurantId) {
+      displayRestaurantDetails(restaurantId);
+    }
+  });
+  
+  function displayRestaurantDetails(restaurantId) {
+    const restaurant = restaurants.find((r) => r.id === Number(restaurantId));
+  
+    if (restaurant) {
+      document.getElementById(
+        "logo"
+      ).innerHTML = `<img class="restaurantImg" src="${restaurant.logo}" alt="${restaurant.name} Logo">`;
+      document.getElementById("name").textContent = restaurant.name;
+      document.getElementById(
+        "location"
+      ).textContent = `Location: ${restaurant.location}`;
+      document.getElementById(
+        "rating"
+      ).textContent = `Rating: ${restaurant.rating}`;
+      document.getElementById("type").textContent = `Type: ${restaurant.type}`;
+      document.getElementById(
+        "desc"
+      ).textContent = `Description: ${restaurant.desc}`;
+    }
+  }
+  
+  const form = document.querySelector(".reviewForm");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const review = e.target.review.value ?? null;
+  
+    if (review) {
+      const reviewContainer = document.querySelector(".reviewsContainer");
+      const singleReview = document.createElement("div");
+      singleReview.classList.add("singleReview");
+      singleReview.innerHTML = createReviewElement(review);
+      reviewContainer.appendChild(singleReview);
+  
+      e.target.review.value = "";
+    } else {
+      alert("Please enter a review before submitting.");
+    }
+  });
+  
+  function createReviewElement(review) {
+    return `
+        <div id="review">
+          ${review}
+        </div>
+        <div class="rating">
+                <img
+                  class="icon"
+                  src="/images/rating.png"
+                  alt="star icon"
+                  width="15"
+                  height="15"
+                />
+                <span id="rating">4.5</span>
+              </div>
+    `;
+  }
